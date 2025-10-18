5778b63e8e4b2e51cffcc5461958fccec9ef785c
Christbru <christbru@live.com>
2025-10-18 18:07:39 -0500

    Correcting branch case issue for deployment building.

--- web-app/src/app/index.jsx
+++ web-app/src/app/index.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD:src/app/index.jsx
-import ChatLayout from "../components/ui/ChatLayout";
-=======
 import ChatLayout from "src/components/layouts/chat-layout";
->>>>>>> cb9cff44215b6de81ed81ef2a1c6abe090fbf1b1:web-app/src/app/index.jsx
 
 function App() {
   return (
