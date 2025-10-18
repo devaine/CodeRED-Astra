85e65b6e75e2bd545835b6bad9057439a26932f0
JK-le-dev <jkcondor13@gmail.com>
2025-10-18 14:44:28 -0500

    fix(tailwindcss): tailwindcss works with vite, no custom theme

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <p className="text-3xl font-bold text-primary">Hello, world!</p>
+    </div>
+  );
+}
+
+export default App;
