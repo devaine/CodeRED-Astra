3429c764e80f18cb25534d744830c72c2ee7b963
JK-le-dev <jkcondor13@gmail.com>
2025-10-18 15:03:25 -0500

    attempt to fix tailwindcss utlity class but no worky

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <p className="text-3xl font-bold text-primary">Hello, world!</p>
+      <p className="text-3xl font-bold text-white">Hello, world!</p>
     </div>
   );
 }
