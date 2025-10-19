778504797616e0ae64c3318c23921fbbf248d662
JK-le-dev <jkcondor13@gmail.com>
2025-10-19 04:32:32 -0500

    feat(file input): adding feature to add and remove files
    Next up is to create a seperate menu that will show all the files.
    Probably as a seperate frame. Needs to be scrollable and can use similar
    ui as before

--- web-app/src/components/ui/button/delete-button.jsx
+++ web-app/src/components/ui/button/delete-button.jsx
@@ -5,7 +5,7 @@ export default function FlameButton({ onClick }) {
   return (
     <motion.button
       onClick={onClick}
-      className="bg-gray-700 p-2 rounded-2xl"
+      className="bg-gray-700 cursor-pointer p-2 rounded-2xl border-2 border-gray-600"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
     >
--- web-app/src/components/ui/chat/chat-header.jsx
+++ web-app/src/components/ui/chat/chat-header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import DeleteButton from "src/components/ui/button/delete-button";
+import SchematicButton from "../button/schematic-button";
 
 export default function ChatHeader({ title = "Title of Chat" }) {
   return (
     <div className="w-full flex justify-center">
-      <header className="text-slate-100 fixed top-4 ">
-        <div>
+      <header className="text-slate-100 fixed top-4 max-w-3xl w-full px-4">
+        <div className="flex justify-between items-center gap-4">
+          <SchematicButton></SchematicButton>
           <h1 className="text-lg font-semibold shadow-md shadow-indigo-600 bg-gray-900 px-6 py-2 rounded-4xl border-2 border-gray-800">
             {title}
           </h1>
+          <DeleteButton></DeleteButton>
         </div>
       </header>
     </div>
