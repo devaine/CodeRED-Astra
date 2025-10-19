11e7d9b14038f0cf502e9d1a7561c04e2647e3ef
yenminh269 <hhyenminh2001@gmail.com>
2025-10-19 03:24:19 -0500

    reverting to original padding

--- web-app/src/components/ui/button/delete-button.jsx
+++ web-app/src/components/ui/button/delete-button.jsx
@@ -5,7 +5,7 @@ export default function FlameButton({ onClick }) {
   return (
     <motion.button
       onClick={onClick}
-      className="bg-gray-700 p-3 rounded-2xl"
+      className="bg-gray-700 p-2 rounded-2xl"
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
     >
